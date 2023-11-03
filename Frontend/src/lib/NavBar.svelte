fe71384cdf0a9c11be5d4ed0f744252bd442666d
KyanPepper <kyankks1@gmail.com>
2023-11-03 01:18:33 -0700

    minor css fix in nav

--- Frontend/src/lib/NavBar.svelte
+++ Frontend/src/lib/NavBar.svelte
@@ -18,7 +18,7 @@
           <img class="h-8 w-auto" src="src\lib\Logo.png">
         </div>
           <div class=" flex space-x-4">
-            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Map</a>
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
