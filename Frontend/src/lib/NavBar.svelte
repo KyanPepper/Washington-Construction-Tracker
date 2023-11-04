4c90c4a0e91cefcc9939aee6417e840bd0a3dc34
KyanPepper <kyankks1@gmail.com>
2023-11-04 01:40:22 -0700

    Added links to nav

--- Frontend/src/lib/NavBar.svelte
+++ Frontend/src/lib/NavBar.svelte
@@ -18,10 +18,10 @@
           <img class="h-8 w-auto" src="src\lib\Logo.png">
         </div>
           <div class=" flex space-x-4">
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Map</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+            <a href="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
+            <a href="projects" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+            <a href="map" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Map</a>
+            <a href="about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
         </div>
       </div>
 </nav>
---
+++ Frontend/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import NavBar from "$lib/NavBar.svelte";
+    import '../app.css';
+    </script>
+  <header>
+  <NavBar>
+    </NavBar>
+  </header>
