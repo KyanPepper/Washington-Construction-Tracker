97647165366a32dcfa3185611549ee22a49ccf5f
KyanPepper <kyankks1@gmail.com>
2023-11-10 20:52:49 -0800

    project page v2

--- Frontend/src/routes/projects/+page.svelte
+++ Frontend/src/routes/projects/+page.svelte
@@ -1,24 +1,16 @@
+<nav class="bg-white shadow flex items-center justify-between p-4 mx-auto max-w-screen-lg font-sans">
+    <div class="flex items-center">
+        <a class="mr-4">Seattle</a>
+        <a class="mr-4">Bellevue</a>
+        <a class="mr-4">Everett</a>
+        <a class="mr-4">Tacoma</a>
+        <a class="mr-4">Spokane</a>
+        <a class="mr-4">Pullman</a>
+        <a class="mr-4">Vancouver</a>
+        <a class="mr-4">Tri Cities</a>
+    </div>
 
-<nav class="shadow-white">
-
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex flex-shrink-0 items-center">
-        </div>
-          <div class="bg-center">
-            <a>Seattle</a>
-            <a>Bellevue</a>
-            <a>Everett</a>
-            <a>Tacoma</a>
-
-            
-            <a>Search it</a>
-          
-          
-            <a>Spokane</a>
-            <a>Pullman</a>
-            <a>Vancouver</a>
-            <a>Tri Cities</a>
-
-        </div>
-      </div>
+    <div>
+        <input type="text" placeholder="Search it" class="border px-4 py-2 rounded" />
+    </div>
 </nav>
