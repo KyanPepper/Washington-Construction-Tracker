b3a9d41386288ea1ebc7e6aa4fcbdce1923bf4c1
KyanPepper <kyankks1@gmail.com>
2023-12-23 20:17:01 -0800

    Created project model

--- Backend/app/Model/models.py
+++ Backend/app/Model/models.py
@@ -1,2 +1,11 @@
 from app import db
 
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    img = db.Column(db.String(1000))
+    url = db.Column(db.String(1000))
+    price = db.Column(db.Double ,default = 0)
+    started_at = db.Column(db.DateTime)
+    address = db.Column(db.String(255))
+    location = db.Column(db.String(255))
