f4b4edd1ff8c11a44464c538946e0243d21b90e6
KyanPepper <kyankks1@gmail.com>
2023-12-25 18:35:02 -0800

    Changed Project to include description and  expected

--- Backend/app/Model/models.py
+++ Backend/app/Model/models.py
@@ -7,5 +7,7 @@ class Project(db.Model):
     url = db.Column(db.String(1000))
     price = db.Column(db.Double ,default = 0)
     started_at = db.Column(db.DateTime)
+    expected_done = db.Column(db.DateTime)
     address = db.Column(db.String(255))
     location = db.Column(db.String(255))
+    description = db.Column(db.String(1000))
