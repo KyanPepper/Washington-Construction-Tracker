04dc28563d74d56eb3f4eaeb29a31edd350616b8
KyanPepper <kyankks1@gmail.com>
2023-12-25 19:42:17 -0800

    Subdoc + project list later ->json

--- Backend/Scraper/wsdot/wsMajor.py
+++ Backend/Scraper/wsdot/wsMajor.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests 
 
+projects = []
+
+broadUrl = 'https://wsdot.wa.gov'
 url = "https://wsdot.wa.gov/construction-planning/major-projects"
 
 result = requests.get(url)
@@ -10,8 +13,6 @@ viewstring = 'view-content'
 view_content_div = doc.find("div", class_="view-content")
 #print(view_content_div.prettify())
 
-
-
 if view_content_div:
     links = view_content_div.find_all("a", href=True)
 
@@ -19,6 +20,19 @@ if view_content_div:
     for link in links:
         #finds href within the link
         href = link.get("href")  
-        print(href)
+        newUrl = broadUrl+href
+       
+        #print(newUrl)
+        subresult = requests.get(newUrl)
+        subDoc = BeautifulSoup(subresult.text,"html.parser")
+        subcontentdiv = subDoc.find("div",class_ ="content")
+        #print(subcontentdiv.prettify())
+        #search throught text
+        pic = subcontentdiv.find('img src')
+        print(pic)
+
+        break
+
 else:
     print('accsess blocked :( ')
+
