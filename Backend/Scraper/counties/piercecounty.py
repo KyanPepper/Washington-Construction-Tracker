a9e66840a8a71ec450d291710d12e1126ba35332
KyanPepper <kyankks1@gmail.com>
2023-12-27 20:08:54 -0800

    Seleium for pierce JS blocked and Nav bar find for Snoho

--- Backend/Scraper/counties/piercecounty.py
+++ Backend/Scraper/counties/piercecounty.py
@@ -1,12 +1,27 @@
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests 
-
-projects = []
+import time
 
 url = 'https://www.piercecountywa.gov/507/Construction-Projects'
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text,'html.parser')
+# Launch a headless browser using Selenium
+options = webdriver.ChromeOptions()
+options.add_argument('headless')  # Run Chrome in headless mode
+driver = webdriver.Chrome(options=options)  # You might need to provide the path to your WebDriver executable
 
-item = doc.find('div', class_ ='widgetBody')
-print(item.prettify())
+driver.get(url)
+
+# Let the page load
+time.sleep(5)  # Adjust the sleep time based on the page load speed
+
+# Get the page source after it's been dynamically loaded
+html_content = driver.page_source
+
+# Use Beautiful Soup to parse the content
+doc = BeautifulSoup(html_content, 'html.parser')
+
+# Continue with your scraping logic
+# ...
+
+# Close the browser session
+driver.quit()
--- Backend/Scraper/counties/snohomishcounty.py
+++ Backend/Scraper/counties/snohomishcounty.py
@@ -1,1 +1,15 @@
+from bs4 import BeautifulSoup
+import requests 
+
+projects = []
+
 url = 'https://snohomishcountywa.gov/3599/Current-Construction-Projects'
+doc = requests.get(url)
+thedoc = BeautifulSoup(doc.text,'html.parser')
+#print(thedoc)
+navbar = thedoc.find('ol',id = 'secondaryMenusecondaryNav')
+#print(navbar.prettify())
+links = navbar.find_all('a' , class_ = 'navMainItem')
+#print(links)
+
+    
