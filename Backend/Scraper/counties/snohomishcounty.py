ed457a1afc5e54775e0502036152c9ed6394a436
KyanPepper <kyankks1@gmail.com>
2023-12-29 17:23:41 -0800

    fixed sepcial chars being left in stings by striping

--- Backend/Scraper/counties/snohomishcounty.py
+++ Backend/Scraper/counties/snohomishcounty.py
@@ -16,34 +16,35 @@ def scrapesnohomish():
     #print(links)
     for link in links:
         href = broadurl + link.get('href')
-        location = link.get_text()
+        location = link.get_text(strip=True)
         location = re.sub(r'\s*\(\d{4}\)', '', location)
         newdoc = requests.get(href)
         projurl = href
         thenewdoc = BeautifulSoup(newdoc.text, 'html.parser')
-        title = thenewdoc.find('h1').get_text()
+        title = thenewdoc.find('h1').get_text(strip=True)
         newdiv = thenewdoc.find('div', class_ = 'pageStyles')
-        description = newdiv.find('p').get_text()
-        price = newdiv.find('h2',string='Funding').find_next('p').get_text()
-        timeline = newdiv.find('h2',string='Funding').find_next('p').get_text()
+        description = newdiv.find('p').get_text(strip=True)
+        price = newdiv.find('h2',string='Funding').find_next('p').get_text(strip=True)
+        timeline = newdiv.find('h2',string='Schedule').find_next('p').get_text(strip=True)
         tempimg = thenewdoc.find_all('img')
         if '14666' not in tempimg[4].get('src'):
-            img = tempimg[4].get('src')
+            img = broadurl + tempimg[4].get('src')
         else:
-            img =tempimg[3].get('src')
+            img =broadurl + tempimg[3].get('src')
 
         project ={
                 'name' : title,
                 'img' : img,
-                'url' : url,
+                'url' : projurl,
                 'price' : price,
                 'timeline' : timeline,
                 'description' : description,
                 'location' : location
         }
         projects.append(project)
+        print(title)
     return projects
-    
+
         
     
         
--- Backend/Scraper/main.py
+++ Backend/Scraper/main.py
@@ -2,8 +2,14 @@
 from bs4 import BeautifulSoup
 import requests 
 import re
+import pprint
 from counties.snohomishcounty import scrapesnohomish
 from WS.wsMajor import scrapeWsMajor
-from WS.lightRail import lightrailscrape
+from WS.lightRail import scrapelightrail
 
+all_projects = []
 
+all_projects.extend(scrapesnohomish())
+all_projects.extend(scrapeWsMajor())
+all_projects.extend(scrapelightrail())
+pprint.pprint(all_projects)
