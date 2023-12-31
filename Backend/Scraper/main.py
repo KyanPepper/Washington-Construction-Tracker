884b6c4b0a726d5f2d7273cc0fd56364d72f71ff
KyanPepper <kyankks1@gmail.com>
2023-12-30 16:21:57 -0800

    removed unnesscary imports

--- Backend/Scraper/main.py
+++ Backend/Scraper/main.py
@@ -1,10 +1,6 @@
 from Scraper.counties.snohomishcounty import scrapesnohomish
 from Scraper.WS.wsMajor import scrapeWsMajor
 from Scraper.WS.lightRail import scrapelightrail
-from bs4 import BeautifulSoup
-import requests 
-import re
-import pprint
 def scrape_all():
 
     all_projects = []
