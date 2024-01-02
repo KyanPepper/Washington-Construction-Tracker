e387c67b8b6919ca5e38ce100cd0789f33ff6208
KyanPepper <kyankks1@gmail.com>
2024-01-01 20:16:10 -0800

    Project card template v1

--- Frontend/src/lib/projectcard.svelte
+++ Frontend/src/lib/projectcard.svelte
@@ -1,20 +1,24 @@
-<script>
-      import '../app.css';
+<script lang="ts">
+  import '../app.css';
+  const url: string = '';
+  const img: string = 'src/lib/Logo.png';
+  const price: string = '40 Billion - 50 Billion';
+  const description: string = 'this is the project of the ...';
+  const name: string = 'Worm project';
+  const timeline: string = '2089 - 2024';
 </script>
 
-<div class="w-64 mx-auto my-4 bg-white rounded-lg shadow-lg drop-shadow-2xl">
-    <div>
-        <div class="w-64 h-40 bg-white rounded-t-lg">
-        <img class="overflow-auto flex-auto snap-center" src="src\lib\Logo.png" alt="WCPLOGO">
-    </div>
-      <h3 class="text-lg font-semibold text-gray-900">Title</h3>
-      <p class="mt-1 text-sm text-gray-500">Address</p>
-      <div class="mt-4">
-        <p class="text-gray-500 text-sm">
-          <span class="mr-2">Days since started: </span>
-          <span>Total Money invested: </span>
-        </p>
-      </div>
-    </div>
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <a href="{url}">
+      <img class="rounded-t-lg" src="{img}"  />
+  </a>
+  <div class="p-5">
+      <a href="#">
+          <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h6>
+      </a>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{price}</p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{timeline}</p>
   </div>
-  
+</div>
