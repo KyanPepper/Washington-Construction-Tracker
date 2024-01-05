5889642ee5783053868cc42816a756346199c983
KyanPepper <kyankks1@gmail.com>
2024-01-04 20:58:49 -0800

    Homepage center align for mobile

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -15,32 +15,28 @@ console.log(rProjects)
   <h1 class="text-xl md:text-2xl lg:text-3xl font-bold text-center text-white">
     Washington Construction Projects
   </h1>
-  <p
-    class="text-sm md:text-base text-center mt-2 font-semibold text-white opacity-75"
-  >
+  <p class="text-sm md:text-base text-center mt-2 font-semibold text-white opacity-75">
     Explore Washington's Latest Developments
   </p>
 </div>
 
-
-  
-<div class="flex justify-between pt-4 pb-4 flex-wrap">
-  {#each rProjects as project, index (project.id)}
-    <Projectcard
-      class="w-full sm:w-1/3 bg-white rounded-lg shadow-md p-2 mt-2 flex flex-col justify-between"
-      price={project.price}
-      description={project.description}
-      name={project.name}
-      img={project.img}
-      url={project.url}
-      timeline={project.timeline}
-      location={project.location}
-      lon={project.lon}
-      lat={project.lat}
-      county={project.county}
-    ></Projectcard>
-  {/each}
+<div class="mx-auto max-w-screen-lg w-full">
+  <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 justify-center pt-4 pb-4">
+    {#each rProjects as project, index (project.id)}
+      <Projectcard
+        class="bg-white rounded-lg shadow-md p-2 mb-2 mt-2"
+        price={project.price}
+        description={project.description}
+        name={project.name}
+        img={project.img}
+        url={project.url}
+        timeline={project.timeline}
+        location={project.location}
+        lon={project.lon}
+        lat={project.lat}
+        county={project.county}
+      ></Projectcard>
+    {/each}
+  </div>
 </div>
-
-  
 
